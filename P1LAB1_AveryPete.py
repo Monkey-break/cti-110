ea79089da0ef8f7734f069ab9f1913f230334d41
Monkey-break <160199404+Monkey-break@users.noreply.github.com>
2024-02-15 22:05:02 -0500

    Add files via upload

---
+++ P1LAB1_AveryPete.py
@@ -0,0 +1,14 @@
+
+# Pete Avery
+# 2/15/24
+# P1Lab1
+# This assignment covers basic Python input and print functions. It should prompt the user twice and then print a greeting. 
+
+
+print ('Enter your first name:', end = ' ')
+fname = input()
+
+print ('Enter your last name:', end = ' ')
+lname = input()
+
+print ('Hello', fname, lname +'!', 'Welcome to CTI - 110')
